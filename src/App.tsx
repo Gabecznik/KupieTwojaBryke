dfaba1d194a27aecda3de058df17e0fd3b07ff0e
Bartosz Kowalski <bartosz.kowalski@polkomtel.com.pl>
2025-10-15 14:00:36 +0200

    cars-list component with filtering by brand&model

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { CarList } from './components/CarList/CarList'
 import './App.css'
+import cars from '../public/api/cars.json'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchValue, setSearchValue] = useState<string>('')
+
+  const filteredCars = [...cars].filter(c => 
+    c.brand.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase().trim()) 
+    || c.model.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase().trim())
+  )
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    <CarList cars={filteredCars} searchValue={searchValue} setSearchValue={setSearchValue} />
+
   )
 }
 
