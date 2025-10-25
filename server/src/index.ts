3639d6d2bea9b3b45821d079fe123556d62da30b
Bartek <kowalski_1410@wp.pl>
2025-10-25 18:19:45 +0200

    replace api path

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma/index.js";
 
 const app = express();
 const prisma = new PrismaClient();
@@ -12,28 +12,28 @@ app.get("/", (req, res) => res.send("API działa"));
 
 /* Produkty */
 app.get("/products", async (req, res) => {
-  const products = await prisma.product.findMany();
+  const products = await prisma.car.findMany();
   res.json(products);
 });
 
 app.get("/products/:id", async (req, res) => {
   const id = Number(req.params.id);
-  const product = await prisma.product.findUnique({ where: { id } });
+  const product = await prisma.car.findUnique({ where: { id } });
   if (!product) return res.status(404).json({ error: "Not found" });
   res.json(product);
 });
 
 app.post("/products", async (req, res) => {
-  const { name, description, price } = req.body;
-  const product = await prisma.product.create({
-    data: { name, description, price: Number(price) },
+  //const { name, description, price } = req.body;
+  const product = await prisma.car.create({
+    data: req.body,
   });
   res.status(201).json(product);
 });
 
 app.delete("/products/:id", async (req, res) => {
   const id = Number(req.params.id);
-  await prisma.product.delete({ where: { id } });
+  await prisma.car.delete({ where: { id } });
   res.status(204).send();
 });
 
